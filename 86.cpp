6fefd298162baacd3327e54ef292b56cc12ea3ce
SkyrimYun <skyrim-yy@outlook.com>
2021-01-14 11:52:43 +0100

    revisit 89

--- 86.cpp
+++ 86.cpp
@@ -19,30 +19,57 @@ struct ListNode
 class Solution
 {
 public:
+    // ListNode *partition(ListNode *head, int x)
+    // {
+    //     ListNode *left = new ListNode(0);
+    //     ListNode *right = new ListNode(0);
+    //     ListNode *p1 = head;
+    //     ListNode *left_prev = left;
+    //     ListNode *right_prev = right;
+    //     while (p1 != nullptr)
+    //     {
+    //         if (p1->val < x)
+    //         {
+    //             left->next = p1;
+    //             left = left->next;
+    //         }
+    //         else
+    //         {
+    //             right->next = p1;
+    //             right = right->next;
+    //         }
+    //         p1 = p1->next;
+    //     }
+    //     right->next = nullptr;
+    //     left->next = right_prev->next;
+    //     return left_prev->next;
+    // }
     ListNode *partition(ListNode *head, int x)
     {
+        ListNode *left_f = nullptr, *right_f = nullptr;
         ListNode *left = new ListNode(0);
         ListNode *right = new ListNode(0);
-        ListNode *p1 = head;
-        ListNode *left_prev = left;
-        ListNode *right_prev = right;
-        while (p1 != nullptr)
+        while (head != nullptr)
         {
-            if (p1->val < x)
+            if (head->val < x)
             {
-                left->next = p1;
+                left->next = head;
+                if (left_f == nullptr)
+                    left_f = head;
                 left = left->next;
             }
             else
             {
-                right->next = p1;
+                right->next = head;
+                if (right_f == nullptr)
+                    right_f = head;
                 right = right->next;
             }
-            p1 = p1->next;
+            head = head->next;
         }
+        left->next = right_f;
         right->next = nullptr;
-        left->next = right_prev->next;
-        return left_prev->next;
+        return left_f;
     }
 };
 
--- 88.cpp
+++ 88.cpp
@@ -19,14 +19,25 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
+        //倒序操作
+        //如果nums1>nums2，在nums1内部改变元素顺序
+        //如果nums1<=nums2，把nums2元素插入nums1
         int i = m + n - 1;
         m--;
         n--;
-        while (n >= 0)
+        while (n >= 0) //因为是 num2插入num1，所以只要num2里还有元素就要往里面插
         {
-            while (m >= 0 && nums1[m] > nums2[n])
-                swap(nums1[i--], nums1[m--]);
+            while (m >= 0 && nums1[m] > nums2[n]) //m>=0是为了那种m已经空了但n还有元素要往里面放的情况
+                swap(nums1[i--], nums1[m--]);     //把0放到该放的地方去
             swap(nums1[i--], nums2[n--]);
         }
     }
 };
+
+int main()
+{
+    Solution s;
+    vector<int> nums1{1, 2, 3, 0, 0, 0};
+    vector<int> nums2{2, 5, 6};
+    s.merge(nums1, 3, nums2, 3);
+}
